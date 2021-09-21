e58119672fa892b6e1b53872fc005495b3d5b060
geyerbisschoff <geyerbisschoff@deloitte.co.uk>
2021-09-21 19:23:10 +0100

    improve TransitionMatrix algorithm to use numpy rather than base Python

--- src/z_model/transition_matrix.py
+++ src/z_model/transition_matrix.py
@@ -1,7 +1,7 @@
 from scipy.stats import norm as normal
 from functools import reduce
 from scipy.linalg import fractional_matrix_power
-from numpy import array, zeros, identity, append, sum, stack
+from numpy import array, sum, abs, cumsum, newaxis, diff
 from pandas import Series, DatetimeIndex
 from dateutil.relativedelta import relativedelta
 
@@ -48,50 +48,20 @@ class TransitionMatrix:
             return reduce(lambda a, x: a @ x if len(a) > 0 else x, l)
 
     @classmethod
-    def from_assumption(cls, ttc_transition_matrix: array, rho: float, z: Series, default_state: int = -1, freq: int = 12, delta: float = 10**-8, **kwargs):
+    def from_assumption(cls, ttc_transition_matrix: array, rho: float, z: Series, freq: int = 12, **kwargs):
 
         def fraction_matrix(x, freq):
-            temp = fractional_matrix_power(x, 1 / freq)
-            temp[temp < 0] = 0
-            return standardise(temp)
+            rs = fractional_matrix_power(x, 1 / freq)
+            rs[rs < 0] = 0
+            rs = abs(rs) / sum(abs(rs), axis=1, keepdims=True)
+            return rs
 
-        def standardise(x, delta=.0):
-            return x / (sum(x, axis=1, keepdims=True) * (1+delta))
+        ttc = fraction_matrix(ttc_transition_matrix, freq)
+        cttc = cumsum(ttc, axis=1)
+        cttc[cttc > 1] = 1
+        cttc[cttc < 0] = 0
 
-        def default_distance(ttc_transition_matrix, default_state, delta):
-            return -normal.ppf(ttc_transition_matrix[:, default_state] - delta)
-
-        def default_barrier(ttc_transition_matrix, default_state, delta):
-            n = len(ttc_transition_matrix)
-            default_distance_vector = default_distance(ttc_transition_matrix, default_state, delta)
-
-            default_barrier_matrix = zeros((n, n))
-            for i in range(n):
-                default_barrier_matrix[:, i] = normal.ppf(sum(ttc_transition_matrix[:, i:], axis=1) - delta) + default_distance_vector
-
-            return default_barrier_matrix
-
-        def z_default_distance(default_distance_vector, rho, z):
-            return default_distance_vector + z * (rho ** 0.5) / (1 - rho) ** 0.5
-
-        def transition_matrix(default_barrier_matrix, z_dd, default_state, delta):
-            n = len(default_barrier_matrix)
-            matrix = zeros((n, n))
-
-            for i in range(n - 1):
-                matrix[:, i] = normal.cdf(default_barrier_matrix[:, i], z_dd) - normal.cdf(default_barrier_matrix[:, i+1], z_dd)
-            matrix[:, default_state] = normal.cdf(default_barrier_matrix[:, default_state], z_dd) + delta
-            return standardise(matrix)
-
-        ttc_transition_matrix = fraction_matrix(standardise(array(ttc_transition_matrix), delta), freq)
-        default_distance_vector = default_distance(ttc_transition_matrix, default_state, delta)
-        default_barrier_matrix = default_barrier(ttc_transition_matrix, default_state, delta)
-        return cls(
-            Series(
-                [
-                    transition_matrix(default_barrier_matrix, z_default_distance(default_distance_vector, rho, z_i), default_state, delta)
-                    for z_i in z
-                ],
-                index=z.index
-            )
-        )
+        default_distance = normal.ppf(cttc)
+        za = z.values[:, newaxis, newaxis]
+        pit = diff(normal.cdf(default_distance + za * (rho ** 0.5) / (1 - rho) ** 0.5), prepend=0)
+        return cls(Series(list(pit), index=z.index))
