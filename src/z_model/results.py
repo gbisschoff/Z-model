5b298ab8e53c370a0325f9347d4c92a2f15021bd
geyerbisschoff <geyerbisschoff@deloitte.co.uk>
2022-07-06 09:01:16 +0100

    change the way results are written to the Zip. This is to ensure that the writer is closed before the zip is closed.

--- src/z_model/results.py
+++ src/z_model/results.py
@@ -6,22 +6,22 @@ from .file_reader import guess_extension
 
 
 class Results:
-    '''
+    """
     Z-Model Results class
 
-    The :class:`Executor` resturns a :class:`Results` object that contains the detailed ECL results and itermediate
+    The :class:`Executor` returns a :class:`Results` object that contains the detailed ECL results and intermediate
     parameters at an account level. The :class:`Results` object also has the ability to summarise the results and
     parameters. Finally, it is possible to save all three reports into a compressed Zip archive for storage and use
     in other programs.
 
-    '''
+    """
     def __init__(self, data: DataFrame):
         self.data = data
 
     def reporting_rate_results(self):
-        '''
+        """
         Return only the IFRS9 Reporting date results
-        '''
+        """
         rs = self.data[
             (self.data['account_type']=='Actual')
             & (self.data['T']==0)
@@ -30,13 +30,13 @@ class Results:
         return rs
 
     def summarise(self, by=('account_type', 'segment_id', 'forecast_reporting_date', 'scenario'), *args, **kwargs):
-        '''
+        """
         Summarise the ECL results.
 
         :param by: a list of columns to summarise by. (Default: ``['account_type', 'segment_id', 'scenario', 'forecast_reporting_date']``)
         :param args, kwargs: Not currently used
 
-        '''
+        """
         df = self.data[[*by, 'EAD(t)', 'P(S=1)', 'P(S=2)', 'P(S=3)', 'P(S=WO)', 'STAGE1(t)', 'STAGE2(t)', 'STAGE3(t)']].copy()
 
         # Calculate the stage conditional exposure
@@ -84,13 +84,13 @@ class Results:
         return rs[[*by, 'stage', '#', 'Exposure(t)', 'ECL(t)', 'CR(t)']]
 
     def parameters(self, by=('segment_id', 'forecast_reporting_date', 'scenario'), *args, **kwargs):
-        '''
+        """
         Summarise the parameters.
 
         :param by: a list of columns to summarise by. (Default: ``['segment_id', 'scenario', 'forecast_reporting_date']``)
         :param args, kwargs: Not currently used
 
-        '''
+        """
         df = self.data[[*by, 'P(S=1)', 'P(S=2)', 'P(S=3)', 'Exposure(t)', '12mPD(t)', 'LGD(t)']].copy()
         df['N'] = (df['P(S=1)'] + df['P(S=2)'] + df['P(S=3)']) # Calculate the expected number of observations
         df['EPD'] = df['Exposure(t)'] * df['12mPD(t)'] # Calculate the exposure weighted PD
@@ -114,19 +114,19 @@ class Results:
         return rs[[*by, '#', 'Exposure(t)', '12mPD(t)', 'LGD(t)']]
 
     def save(self, url: Path, *args, **kwargs):
-        '''
+        """
         Save the results and reports to a zip archive.
 
         :param url: the path to a Zip archive.
         :param args: arguments passes to summarise & parameters
         :param kwargs: arguments passes to summarise & parameters
 
-        '''
+        """
         if guess_extension(url) != '.zip':
             raise ValueError(f'The file path {url} is not a .zip file.')
 
         with ZipFile(url, mode="w", compression=ZIP_DEFLATED, compresslevel=9) as zf:
-            self.data.to_csv(zf.open('detailed-results.csv', 'w'), index=False)
-            self.reporting_rate_results().to_csv(zf.open('reporting-date-results.csv', 'w'), index=False)
-            self.summarise(*args, **kwargs).to_csv(zf.open('summary.csv', 'w'), index=False)
-            self.parameters(*args, **kwargs).to_csv(zf.open('parameters.csv', 'w'), index=False)
+            zf.writestr("detailed-results.csv", self.data.to_csv(index=False))
+            zf.writestr("reporting-date-results.csv", self.reporting_rate_results().to_csv(index=False))
+            zf.writestr("summary.csv", self.summarise(*args, **kwargs).to_csv(index=False))
+            zf.writestr("parameters.csv", self.parameters(*args, **kwargs).to_csv(index=False))
